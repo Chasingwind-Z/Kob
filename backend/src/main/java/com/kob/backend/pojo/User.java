3433069caf5fb9b62d297114bb508c95182411a8
zhifei zhu <1367488808@qq.com>
2023-02-10 22:36:56 +0800

    配置MySQL与注册登录模块

---
+++ backend/src/main/java/com/kob/backend/pojo/User.java
@@ -0,0 +1,15 @@
+package com.kob.backend.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+}
--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,20 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("421"));
+        System.out.println(passwordEncoder.encode("123"));
+        System.out.println(passwordEncoder.encode("456"));
+        System.out.println(passwordEncoder.encode("789"));
     }
 
 }
